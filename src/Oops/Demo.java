d127c40ed5b5c3fbc43299f3bd0da2863ba1a970
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2023-11-11 02:03:28 +0530

    Day 1

---
+++ src/Oops/Demo.java
@@ -0,0 +1,15 @@
+package Oops;
+/*
+## OOPS - java python c++
+### What does object-oriented programming means ?
+- Any programming language Consider as a object-oriented programming language if that follows these features .
+and those features are
+- Encapsulation
+- Inheritance
+- Abstraction
+- Polymorphism
+
+ */
+public class Demo {
+    String feactures;
+}
---
+++ src/Oops/Inheritance.java
@@ -0,0 +1,34 @@
+package Oops;
+/*
+# Inheritance
+
+- if properties from parent class coming to child class then it is called as inheritance.
+
+### Why ?
+reusability
+
+- we use extends keyword in this.
+
+## Types Of Inheritance
+- Single
+- multilevel
+- multiple
+    - super() if the multiple inheritance is present in java than constructor of a child class creates the problem which constructor is to be called.
+    - if both super class have same method than which method is to be inherited in sub class that's create the problem.
+- hybrid
+- hierarchical
+
+- private members doesn't take participate in Inheritance.
+-
+
+ */
+
+public class Inheritance {
+}
+
+class  A{
+   private int a=90;
+}
+class B extends  A{
+
+}
