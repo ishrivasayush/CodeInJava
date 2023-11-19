0b8d541e9aa4fb3c847143c7a1e7f1efe99661a7
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2023-11-19 15:18:21 +0530

    Day 5

---
+++ src/DataStructures/HashMap/HashMapDebug.java
@@ -0,0 +1,54 @@
+package DataStructures.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Map
+-Map is just a collection of key values pairs.
+- keys are unique
+- Map is not an implementation or it does not extend the collection interface. But it is a part of the collection concept.
+- Map is a collection of key-value pairs. in this, for every value of an element, a key is associated with it.
+- Map itself is in an interface that supports key-value pairs. A class that implements it is called HashMap.
+- We can also use a HashTable instead of using a HashMap.
+- The difference between HashMap and HashTable is that HashTable is synchronized while HashMap is normal.
+- If you are working with multiple threads at a time, then it is better to use a HashTable to make it synchronized.
+- remove() method is used to delete a particular element or an entity from a map.
+- We can add elements in a map using the put() method that accepts both the key and value of it.
+- We can get the value of an element by specifying its key of it in the get() method.
+- Keys are unique but values can be repeated. The value of a key can also be updated.
+- In a map, keys are in a set while values are in a list.
+- We can get all the keys by using the method keySet().
+- We can get the value of a key by using the method get() and pass a parameter as a key in the method.
+- remove() method is used to delete a particular element or an entity from a map.
+
+ */
+public class HashMapDebug {
+    public static void main(String[] args) {
+        Map<String,Integer> map=new HashMap<>();
+
+        map.put("aaa",1);
+        map.put("bbb",2);
+        map.put("ccc",3);
+        map.put("ddd",4);
+
+        map.remove("aaa");
+
+        System.out.println(map.containsKey("aaa"));
+        System.out.println(map.containsValue(5));
+        System.out.println(map.keySet());
+        System.out.println(map.isEmpty());
+        System.out.println(map.size());
+
+
+        System.out.println(map);
+        System.out.println(map.get("aaa"));
+
+        System.out.println("Print data.");
+
+        for (String key:map.keySet()) {
+            System.out.println(key+":"+ map.get(key));
+        }
+
+    }
+}
